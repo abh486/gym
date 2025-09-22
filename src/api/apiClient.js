3b2e8075b6cf573918c5d6c6763dc7c268b5702a
copym-xyz <support@copym.xyz>
2025-09-22 17:10:03 +0530

    Your descriptive commit message

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -97,7 +97,7 @@ const auth0 = new Auth0({
 });
 
 const apiClient = axios.create({
-  baseURL: " https://5d361da3675a.ngrok-free.app/api",
+  baseURL: "https://0b9eae409e78.ngrok-free.app/api",
   timeout: 15000,
   headers: { "Content-Type": "application/json" },
 });
